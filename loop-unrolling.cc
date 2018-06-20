c3bf265985c385137d4b9a117b5ccd20883d54d0
Leo Xu <enpengxu@gmail.com>
2018-06-20 18:54:44 -0400

    update

--- loop-unrolling.cc
+++ loop-unrolling.cc
@@ -2,40 +2,86 @@
 #include <string>
 #include <vector>
 
-std::string A = "abcde";
-std::string B = "12345";
-std::string C = "ABCDE";
+std::string A = "abcdefg";
+std::string B = "1234567890";
+std::string C = "ABCDEFG";
+std::string D = "+-=/*";
+std::string E = "!@#$%^&";
 
-std::vector<std::string> S(3);
+
+std::vector<std::string> S2 = { A, B};
+std::vector<std::string> S3 = { A, B, C};
+std::vector<std::string> S4 = { A, B, C, D};
+std::vector<std::string> S5 = { A, B, C, D, E};
 
 void
-test1()
+test_loops2(std::vector<std::string> & S)
 {
-	for (auto a: A ){
-		for (auto b: B ){
-			for (auto c: C ){
+	for (auto a: S[0] ){
+		for (auto b: S[1] ){
+			std::cout << a << " " << b << " " << std::endl;
+		}
+	}
+}
+
+void
+test_loops3(std::vector<std::string> & S)
+{
+	for (auto a: S[0] ){
+		for (auto b: S[1] ){
+			for (auto c: S[2] ){
 				std::cout << a << " " << b << " " << c << " " << std::endl;
 			}
 		}
 	}
 }
 
-void loop()
+void
+test_loops4(std::vector<std::string> & S)
 {
-	std::vector<int> I(3);
-	std::vector<int> N(3);
+	for (auto a: S[0] ){
+		for (auto b: S[1] ){
+			for (auto c: S[2] ){
+				for (auto d: S[3] ){
+					std::cout << a << " " << b << " " << c << " " << d << " " << std::endl;
+				}
+			}
+		}
+	}
+}
 
-	N[0] = A.size();
-	N[1] = B.size();
-	N[2] = C.size();
+void
+test_loops5(std::vector<std::string> & S)
+{
+	for (auto a: S[0] ){
+		for (auto b: S[1] ){
+			for (auto c: S[2] ){
+				for (auto d: S[3] ){
+					for (auto e: S[4] ){
+						std::cout << a << " " << b << " " << c << " " << d << " " << e << " " << std::endl;
+					}
+				}
+			}
+		}
+	}
+}
 
-	S[0] = A;
-	S[1] = B;
-	S[2] = C;
+void loop(std::vector<std::string> & S)
+{
+	int loops = S.size();
+	std::vector<int> I(loops);
+	std::vector<int> N(loops);
 
-	int NN = 1, loops = 3;
+	for (int i=0; i<loops; i++)
+		N[i] = S[i].size();
+
+	int NN = 1;;
 	for (auto n : N)
 		NN *= n;
+
+	// Ii = Mi % Ni
+	// Mi-1 = Mi / Ni;
+	// Mn-1 = k
 	int M = 0;
 	for (int k=0; k<NN; k++ ){
 		M = k;
@@ -49,15 +95,22 @@ void loop()
 		}
 		std::cout << std::endl;
 	}
-
 }
 
 
 int main(int argc, char ** argv)
 {
-	if (argv[1][0] == '0')
-		test1();
-	else
-		loop();
+	if (argv[1][0] == '0') {
+		test_loops2(S2);
+		test_loops3(S3);
+		test_loops4(S4);
+		test_loops5(S5);
+	}
+	else {
+		loop(S2);
+		loop(S3);
+		loop(S4);
+		loop(S5);
+	}
 	return 0;
 }
